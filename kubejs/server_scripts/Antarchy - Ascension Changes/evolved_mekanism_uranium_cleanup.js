a613c6d97382a388a038ccb7438cd6001041fb15
CtrlAltDyleted <203191368+CtrlAltDyleted@users.noreply.github.com>
2026-08-22 00:35:16 -0500

    Remove unwanted Evolved Mekanism uranium recipes
    Remove the non-generating netherrack, End Stone, holystone, depthrock, and shiverstone uranium processing recipes to keep uranium unified around the intended pack ores.

---
+++ kubejs/server_scripts/Antarchy - Ascension Changes/evolved_mekanism_uranium_cleanup.js
@@ -0,0 +1,7 @@
+ServerEvents.recipes(event => {
+    event.remove({ id: 'evolvedmekanism:processing/uranium/ore/depthrock_from_raw' })
+    event.remove({ id: 'evolvedmekanism:processing/uranium/ore/end_stone_from_raw' })
+    event.remove({ id: 'evolvedmekanism:processing/uranium/ore/holystone_from_raw' })
+    event.remove({ id: 'evolvedmekanism:processing/uranium/ore/netherrack_from_raw' })
+    event.remove({ id: 'evolvedmekanism:processing/uranium/ore/shiverstone_from_raw' })
+})
